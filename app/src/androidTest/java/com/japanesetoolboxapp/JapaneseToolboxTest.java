0914f1a88d4343124dd4b187efba604782cb1e70
Bar Lr <bar.larom@gmail.com>
2018-02-05 17:23:45 +0200

    Added Tesseract OCR and image crop functionality.

--- app/src/androidTest/java/com/japanesetoolboxapp/JapaneseToolboxTest.java
+++ app/src/androidTest/java/com/japanesetoolboxapp/JapaneseToolboxTest.java
@@ -5,32 +5,42 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
-import static android.support.test.espresso.Espresso.getIdlingResources;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
-
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class JapaneseToolboxUITest {
+public class JapaneseToolboxTest {
 
     @Rule
     public ActivityTestRule<SplashScreen> mActivityTestRule = new ActivityTestRule<>(SplashScreen.class);
 
     @Test
-    public void japaneseToolboxUITest() {
+    public void JapaneseToolboxTest() {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(1500);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -72,7 +82,9 @@ public class JapaneseToolboxUITest {
 
         TestKanjiDecomposition("言", "亠");
 
+
     }
+
     public void TestInputStringAndButton(String button_string, String input) {
 
         ViewInteraction autoCompleteTextView = onView(
@@ -238,4 +250,5 @@ public class JapaneseToolboxUITest {
                 allOf(withText(kanji_level1), isDisplayed()));
         textView.perform(click());
     }
+
 }
